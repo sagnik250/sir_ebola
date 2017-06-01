5d29e734b74895f0ac4f8c61d1ba7a3ac32cec5b
sagnik250 <sagnik250@gmail.com>
2017-06-01 21:39:31 +0530

    Add files via upload
    Python code for SIR simulation using Euler's method for numerical integration.

---
+++ SIR.py
@@ -0,0 +1,33 @@
+import numpy as np
+import math
+
+
+h = float(input("Enter time-step value"))
+duration = float(input("Enter the duration of simulation"))
+N = int(duration/h)
+
+beta = float(input("Enter value of beta"))
+gamma =  float(input("Enter value of gamma"))
+
+population = np.zeros(3)
+
+print "Enter initial population of susceptibles, infected, and recovered individuals respectively"
+
+for i in range(3):
+	
+	population[i] = int(input(""))
+
+def SIR():
+
+	for i in range(N) :
+
+		population[0] -= population[0] * population[1] * h * beta
+
+		population[1] += (population[0] * population[1] * h * beta) - (population[1] * h * gamma)
+
+		population[2] += population[1] * h * gamma
+
+		print i , population[0], population[1], population[2] 
+
+SIR()  
+	
